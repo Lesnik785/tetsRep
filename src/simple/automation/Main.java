05d5b42327709fca487198cae218a89967100c48
Lesni <lesnik785@gmail.com>
2021-09-12 11:00:50 +0300

    Test

---
+++ src/simple/automation/Main.java
@@ -0,0 +1,22 @@
+package simple.automation;
+
+public class Main {
+
+    private static final String EXISTED_USER = "existed";
+    private static final String NOT_EXISTED_USER = "wrong";
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    private  boolean loginAs(String login, String password) {;
+
+        if (login.equals(EXISTED_USER)){
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
