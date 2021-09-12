d00f82586375dde6d1ba4c54322229cffa552434
Lesni <lesnik785@gmail.com>
2021-09-12 11:10:14 +0300

    Test3

--- src/simple/automation/Main.java
+++ src/simple/automation/Main.java
@@ -5,8 +5,7 @@ public class Main {
     private static final String EXISTED_USER = "existed";
     private static final String NOT_EXISTED_USER = "wrong";
 
-    private  Integer test = 1;
-
+    private Integer test = 1;
     public static void main(String[] args) {
 	// write your code here
     }
